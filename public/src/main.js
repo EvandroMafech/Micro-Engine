e7fa0fe872238ae57e951695a525b06e59e0f527
Evandro Magalhães Ferreira Chagas <evandrochagas_95@hotmail.com>
2025-08-23 14:17:32 -0300

    Atualizando códigos

--- public/src/main.js
+++ public/src/main.js
@@ -4,7 +4,7 @@ import Tile from "./components/Tile.js";
 import {tileSetCanvasFrameInfo} from "./logic/tilesetCanvas.js";
 import Player from "./components/Player.js";
 import Fruits from "./components/Fruits.js";
-import Saw from "./components/saw_temp.js";
+import Saw from "./components/Saw.js";
 import Spykes from "./components/Spykes.js";
 import Trampoline from "./components/Trampoline.js";
 import Platform from "./components/Platform.js";
