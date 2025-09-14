908007ca95b15e16789c8459baf3d7a49f957688
Evandro Magalhães Ferreira Chagas <evandrochagas_95@hotmail.com>
2025-09-14 18:40:02 -0300

    deploy

--- public/src/core/utils/constants.js
+++ public/src/core/utils/constants.js
@@ -1,4 +1,4 @@
-export const tileSetSpriteheet_image_path = "public/assets/images/Terrain/Terrain (16x16) vertical.png.png"
+export const tileSetSpriteheet_image_path = "/assets/images/Terrain/Terrain (16x16) vertical.png"
 const chain = new Image()
 
 chain.src = "public/assets/images/Traps/Spiked Ball/Chain.png.png"
