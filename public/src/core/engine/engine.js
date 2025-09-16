a953b17769488d9da5e362e4b2048b4e02af1e1b
Evandro Magalhães Ferreira Chagas <evandrochagas_95@hotmail.com>
2025-09-16 20:31:42 -0300

    deploy

--- public/src/core/engine/engine.js
+++ public/src/core/engine/engine.js
@@ -67,7 +67,7 @@ const tileSize = 64*3
 
 export function setImageOnBackgroundTiles(image){ //posiciona os quadrados de background no canvas
     const bgImage = image ?? activeSelectedImage.imageUrl;
-    activeBackgroundImage.push(bgImage)
+    activeBackgroundImage.push("/" + bgImage)
     console.log(activeBackgroundImage[activeBackgroundImage.length-1])
     backgroundArray.forEach(tile => {tile.drawBackground(bgImage)})
 }
